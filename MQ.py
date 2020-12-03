f137c6a231d4a53ca9b8cda83798e17bc31fded8
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-03 01:32:50 -0500

    Update MQ.py

--- MQ.py
+++ MQ.py
@@ -2,8 +2,8 @@ import time
 import spidev
 
 # Sensor channel on MCP3008
-#Alcohol_CHANNEL = 0
-NO2_CHANNEL = 1 #This one should be changed properly later
+Alcohol_CHANNEL = 0 #MQ3
+#NO2_CHANNEL = 1 #This one should be changed properly later
 #CH4_CHANNEL = 2
 #LPG_CHANNEL = 3
 #CO_CHANNEL = 4
@@ -19,9 +19,10 @@ pullup = 10000
 #CO_Conversions = [((0, 100), (0, 0.25)), ((100, 133), (0.25, 0.325)),
 #    ((133, 167), (0.325, 0.475)), ((167, 200), (0.475, 0.575)),
 #    ((200, 233), (0.575, 0.665)), ((233, 267), (0.666, 0.75))]
-NO2_Conversions = [((0, 100), (0, 0.25)), ((100, 133), (0.25, 0.325)),
-    ((133, 167), (0.325, 0.475)), ((167, 200), (0.475, 0.575)),
-    ((200, 233), (0.575, 0.665)), ((233, 267), (0.666, 0.75))]
+# Conversions based on Rs/Ro vs mg/L plots of the MQ3 gas sensor
+Alcohol_Conversions = [((0.1, 0.22), (2.2, 1.6)), ((0.22, 4), (1.6, 1.0)),
+    ((0.4, 1.0), (1.0, 0.53)), ((1.0, 1.7), (0.53, 0.4)),
+    ((1.7, 2.6), (0.4, 0.28)), ((2.6, 4), (0.28, 0.2))]
 
 # Open SPI bus
 spi = spidev.SpiDev()
@@ -47,14 +48,14 @@ def converttoppm(rs, conversions):
         if a[0][0] >= rsper > a[0][1]:
             mid, hi = rsper - a[0][0], a[0][1] - a[0][0]
             sf = float(mid) / hi
-            ppm = sf * (a[1][1] - a[1][0]) + a[1][0]
-            return ppm
+            mg/L = sf * (a[1][1] - a[1][0]) + a[1][0]
+            return mg/L
         return 0
 
-def get_NO2():
-    rs = get_resistance(NO2_CHANNEL)
-    ppm = converttoppm(rs, NO2_Conversions)
-    return ppm
+def get_Alcohol():
+    rs = get_resistance(Alcohol_CHANNEL)
+    mg/L = converttoppm(rs, Alcohol_Conversions)
+    return mg/L
 
 #def get_CO():
 #    rs = get_resistance(CO_CHANNEL)
@@ -63,11 +64,11 @@ def get_NO2():
 
 # Controller main function
 def runController():
-    NO2_reading = get_NO2()
+    Alcohol_reading = get_Alcohol()
 #    CO_reading = get_CO()
 #    print('CO={0:0.5f} ppm  NO2={1:0.5f} ppm'.format(CO_reading, NO2_reading))
-    print('NO2={:0.5f} ppm'.format(NO2_reading))
+    print('Alcohol={:0.5f} mg/L'.format(Alcohol_reading))
 
 while True:
     runController()
-    time.sleep(10)
+    time.sleep(15)
