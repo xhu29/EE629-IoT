4a2bc3a399a09218c8cb4eb65733b33b9b320135
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-04 16:55:12 -0500

    Update example.py

--- example.py
+++ example.py
@@ -5,13 +5,13 @@ try:
     print("Press CTRL+C to abort.")
     
     mq = MQ();
-    while True:
+    """while True:
         perc = mq.MQPercentage()
         sys.stdout.write("\r")
         sys.stdout.write("\033[K")
         #sys.stdout.write("Alcohol: %g ppm" % (perc["GAS_Alcohol"]))
         sys.stdout.flush()
-        time.sleep(3)
+        time.sleep(3)"""
 
 except:
     print("\nAbort by user")
