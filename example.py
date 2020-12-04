d3a0995855567052fc174491ee2cd88cccb42085
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-04 15:43:34 -0500

    Update example.py

--- example.py
+++ example.py
@@ -9,9 +9,9 @@ try:
         perc = mq.MQPercentage()
         sys.stdout.write("\r")
         sys.stdout.write("\033[K")
-        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+        sys.stdout.write("Alcohol: %g mg/L" % (perc["GAS_Alcohol"]))
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(3)
 
 except:
     print("\nAbort by user")
