b37e2d5750268d1eb4a2a3fc4b832308a9d0f9bf
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-04 23:59:31 -0500

    Update mq.py

--- mq.py
+++ mq.py
@@ -8,8 +8,8 @@ class MQ():
 
     ######################### Hardware Related Macros #########################
     MQ_PIN                       = 0        # define which analog input channel you are going to use (MCP3008)
-    RL_VALUE                     = 5      # define the load resistance on the board, in kilo ohms
-    RO_CLEAN_AIR_FACTOR          =9.83     # RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+    RL_VALUE                     = 200      # define the load resistance on the board, in kilo ohms
+    RO_CLEAN_AIR_FACTOR          = 10     # RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
                                             # which is derived from the chart in datasheet
  
     ######################### Software Related Macros #########################
@@ -28,7 +28,7 @@ class MQ():
         self.MQ_PIN = analogPin
         self.adc = MCP3008()
         
-        self.AlcoholCurve = [2.3,0.21,-0.47]    # two points are taken from the curve. 
+        self.AlcoholCurve = [-1,0.0792,-0.5]# two points are taken from the curve. 
                                             # with these two points, a line is formed which is "approximately equivalent"
                                             # to the original curve. 
                                             # data format:{ x, y, slope}; point1: (lg0.1, 0.0792), point2: (lg10, -0.9208) 
@@ -54,7 +54,7 @@ class MQ():
     ############################################################################ 
     def MQResistanceCalculation(self, raw_adc):
         print("it is ok")
-        return self.RL_VALUE*(1023.0-raw_adc)/raw_adc;
+        return float(self.RL_VALUE*(1023.0-raw_adc))/float(raw_adc); 
      
      
     ######################### MQCalibration ####################################
@@ -68,8 +68,7 @@ class MQ():
     def MQCalibration(self,MQ_PIN):
         val = 0.0
         for i in range(self.CALIBARAION_SAMPLE_TIMES):          # take multiple samples
-            val += self.MQResistanceCalculation(self.adc.read(MQ_PIN))
-            
+            val += self.MQResistanceCalculation(self.adc.read(MQ_PIN)) 
             time.sleep(self.CALIBRATION_SAMPLE_INTERVAL/1000.0)
             
         val = val/self.CALIBARAION_SAMPLE_TIMES                 # calculate the average value
