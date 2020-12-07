2783ecc7c396d3cc950716d71d303d7b305444ff
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-07 15:11:49 -0500

    Update mqtrial.py

--- mqtrial.py
+++ mqtrial.py
@@ -7,6 +7,11 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 976000
 
+# Define basic parameters of the sensor
+Vin = 5
+RL  = 200    # define the load resistance on the board, in kilo ohms
+RO  = 10     # RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+             # which is derived from the chart in datashee
 # Function to read SPI data from MCP3008 chip
 def ReadChannel(channel):
     adc = spi.xfer2([1, (8+channel) << 4, 0])
@@ -16,7 +21,11 @@ def ReadChannel(channel):
 # Function to read sensor connected to MCP3008
 def readMQ():
     level = ReadChannel(channel)
+#convert the reading based on the datasheet
+    
     return level
+
+
 
 # Controller main function
 def runController():
