0f0bcc4aeac344cc7ce45f3a9deb40ca3ed2f94d
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-09 10:02:43 -0500

    Update mq.py

--- mq.py
+++ mq.py
@@ -53,8 +53,7 @@ class MQ():
     #          could be derived.
     ############################################################################ 
     def MQResistanceCalculation(self, raw_adc):
-        print("it is ok")
-        return float(self.RL_VALUE*(1023.0-raw_adc)/float(raw_adc)); 
+           return float(self.RL_VALUE*(1023.0-raw_adc)/float(raw_adc)); 
      
      
     ######################### MQCalibration ####################################
