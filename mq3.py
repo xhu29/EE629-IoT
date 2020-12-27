aeccc6c0d6a7cfe6a5a90c9d653f115d32c45625
Xi Hu <70543528+xhu29@users.noreply.github.com>
2020-12-27 16:56:29 -0500

    Update mq3.py

--- mq3.py
+++ mq3.py
@@ -39,8 +39,9 @@ def runController():
     sensor_volt = Vout1 / 1024 * 5
     Rs= RL*(5.0 - sensor_volt) / sensor_volt
     Rs_Ro_ratio = Rs/Ro
+    Concentration = math.pow(10, (((math.log(Rs_Ro_ratio) + 0.2891)/0.6316) # The approximately linear regression obtained from the curve on datasheet of each sensor
 
-    return math.pow(10, (((math.log(Rs_Ro_ratio) + 0.2891)/0.6316) # The approximately linear regression obtained from the curve on datasheet of each sensor
+    return Concentration
 
     print('Concentration = {0:0.4f} mg/L'.format(Vout))
 
