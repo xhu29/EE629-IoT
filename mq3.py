7e428d9fcdb4961eeecce606a20ed3807863664f
Xi Hu <70543528+xhu29@users.noreply.github.com>
2021-01-04 16:25:18 -0500

    Update mq3.py

--- mq3.py
+++ mq3.py
@@ -1,7 +1,6 @@
 import time
 import spidev
-
-channel = 0  # Channel '0' is for MQ3(alcohol) gas sensor.
+channel = 0 # Channel '0' is for MQ3(alcohol) gas sensor.
 
 # Open SPI bus
 spi = spidev.SpiDev()
@@ -9,38 +8,25 @@ spi.open(0, 0)
 spi.max_speed_hz = 976000
 
 # Define basic parameters of the sensor
-Vin = 5.0
-Sample_Times = 50 # Determine how many samples will be taken in the calibration phase
-RL = 200  # define the load resistance on the board, in kilo ohms
-#RO = 10  # Assume that RO equals to 10. Theoretically, it should be gained by calibrating the sensor in clean air.
-
-
+Vin = 5
+RL  = 200    # define the load resistance on the board, in kilo ohms
+RO = 10 #Assume that RO equals to 10. Theoretically, it should be gained by calibrating the sensor in clean air.
 def ReadChannel(channel):
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    adc = spi.xfer2([1, (8+channel) << 4, 0])
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
-
-# Function to read sensor connected to MCP3008 and calibrate the sensor
+# Function to read sensor connected to MCP3008
 def readMQ():
     Vout = ReadChannel(channel)
-    for i in range (Sample_Times):
-        Vout += ReadChannel(channel)
-        Vout1 = Vout / Sample_Times
-      sensor_volt = Vout1 / 1024 * 5
-    Rs_air = RL * (Vin - sensor_volt) / sensor_volt
-    Ro = Rs_air / 60.0
-    print("Ro = {0:0.4f} Kohm".format(Ro))
-
+    return Vout
+  
 # Controller main function
 def runController():
     Vout = readMQ()
-    sensor_volt = Vout / 1024 * 5
-    Rs= RL*(Vin - sensor_volt1) / sensor_volt
-    Rs_Ro_ratio = Rs/Ro
-    Concentration = math.pow(10, (((math.log(Rs_Ro_ratio) + 0.2891)/0.6316) # The approximately linear regression obtained from the curve on datasheet of each sensor
-    print('Alcohol = {0:0.4f} mg/L'.format(Concentration))
-
+    Rs = RL*(Vin*1023/Vout - 1)
+    print('Concentration = {0:0.4f} ppm'.format(Vout), ';','Rs = {0:0.4f} kohm'.format(Rs))
+    
 
 while True:
     try:
