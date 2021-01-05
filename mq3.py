f3116dcda9d0553baf30c3b3106471b35b3112a0
Xi Hu <70543528+xhu29@users.noreply.github.com>
2021-01-05 00:42:38 -0500

    Update mq3.py

--- mq3.py
+++ mq3.py
@@ -44,7 +44,7 @@ def runController(Ro):
     Vout = readMQ()
     Rs = RL * (Vin * 1023 / Vout - 1)
     Rs_Ro_Ratio = Rs/Ro
-    Alcohol = 530*pow(10, (2.3220 - math.log10(Rs_Ro_Ratio))/0.2202)
+    Alcohol = 532*pow(10, (2.3220 - math.log10(Rs_Ro_Ratio))/0.2202) #Refer to https://www.nap.edu/read/5435/chapter/11| 1 ppm = 0.00188 mg/L
     print('Alcohol = {0:0.4f} ppm'.format(Alcohol), ';', 'Rs = {0:0.4f} kohm'.format(Rs))
     print (math.log10(Rs_Ro_Ratio))
 
