b95967bafdec48fb4cbbde8430b1493165f27d56
Xi Hu <70543528+xhu29@users.noreply.github.com>
2021-01-11 22:09:16 -0500

    Update mq3.py

--- mq3.py
+++ mq3.py
@@ -15,9 +15,9 @@ RL = 200  # define the load resistance on the board, in kilo ohms
 
 
 #def ReadChannel(channel):
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])
-    data = ((adc[1] & 3) << 8) + adc[2]
-    return data
+    #adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    #data = ((adc[1] & 3) << 8) + adc[2]
+    #return data
 
 
 # Function to read sensor connected to MCP3008
