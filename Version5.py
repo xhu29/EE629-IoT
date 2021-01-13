134ed9375290fd059cb4dc123b24d1ab53439f8a
Xi Hu <70543528+xhu29@users.noreply.github.com>
2021-01-13 17:19:26 -0500

    Update Version5.py

--- Version5.py
+++ Version5.py
@@ -10,7 +10,7 @@ import datetime
 # from system_info import get_temperature
 from oauth2client.service_account import ServiceAccountCredentials
 
-GDOCS_OAUTH_JSON = 'machine-olfactory-project-acd23fe11193.json'
+GDOCS_OAUTH_JSON = 'machine-olfactory-project-13367bc49c29.json'
 GDOCS_SPREADSHEET_NAME = 'Machine Olfactory Project'
 FREQUENCY_SECONDS = 5
 
